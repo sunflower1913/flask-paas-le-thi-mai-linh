7fa22f6858dc70e5157a5e9765c3cafa9b2cb0b2
sunflower1913 <233404050187@ldxh.edu.vn>
2026-09-04 14:03:33 +0700

    feat: them API counterva API info

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ import platform
 
 app = Flask(__name__)
 
+visit_count = 0 # Biến đếm lưu trong RAM của container
+
+
+@app.route("/api/counter")
+def counter():
+    global visit_count
+    visit_count += 1
+    return {
+        "so_lan_truy_cap": visit_count,
+        "ghi_chu": "So nay se MAT khi container khoi dong lai!"
+    }
+
 @app.route("/")
 def home():
     return f"""
@@ -45,6 +57,26 @@ def home():
 @app.route("/health")
 def health():
     return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+@app.route("/api/counter")
+def counter():
+    global visit_count
+    visit_count += 1
+    return {
+        "so_lan_truy_cap": visit_count,
+        "ghi_chu": "So nay se MAT khi container khoi dong lai!"
+    }
+
+@app.route("/api/info")
+def info():
+    ten_sinh_vien = os.environ.get("STUDENT_NAME", "Chua dat bien moi truong")
+    return {
+        "sinh_vien": ten_sinh_vien,
+        "nguon_du_lieu": "Environment Variable tren Render, KHONG hardcode trong code"
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
