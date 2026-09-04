90492ca4f6dbb7fe781b54d1d8d84c3df92a2fbd
sunflower1913 <233404050187@ldxh.edu.vn>
2026-09-04 13:30:43 +0700

    feat: update tieu de v2

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def home():
         </style>
     </head>
     <body>
-        <h1>Ung dung Flask tren PaaS</h1>
+        <h1>Ung dung Flask tren PaaS - phien ban 2!</h1>
         <div class="box">
             <p><b>Sinh vien:</b> LE_THI_MAI_LINH – 233404050187</p>
             <p><b>Mon hoc:</b> Dien toan Dam may</p>
